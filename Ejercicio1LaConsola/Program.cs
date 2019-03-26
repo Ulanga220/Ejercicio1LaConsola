9d1a6517c9f4b43a0d5e4bb2f6217daab6846a2a
S. Ulises Ferreyra <ulises.ferreyra@hotmail.com>
2019-03-26 00:11:06 -0300

    Terminado

--- Ejercicio1LaConsola/Program.cs
+++ Ejercicio1LaConsola/Program.cs
@@ -10,6 +10,15 @@ namespace Ejercicio1LaConsola
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola Mundo");
+            string mensaje;
+            Console.WriteLine("¿Cuál es el club más grande del mundo?");
+            mensaje = Console.ReadLine();
+            Console.WriteLine($"El club elejido fue {mensaje}");
+            Console.ReadKey();
+
+
+
         }
     }
 }
